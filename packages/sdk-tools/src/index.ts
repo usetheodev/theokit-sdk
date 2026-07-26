c51e3699881af023bb2c4a8bdcee3777579d3e94
paulohenriquevn <paulohenriquevn@gmail.com>
2026-07-26 14:34:00 -0300

    fix(m77): exporta TruncationMode no barril (knip acusou export morto) + ignora refactor/

--- packages/sdk-tools/src/index.ts
+++ packages/sdk-tools/src/index.ts
@@ -106,7 +106,12 @@ export { type CreateSearchTextToolOptions, createSearchTextTool } from "./search
 export { type CreateShellToolOptions, createShellTool } from "./shell-exec.js";
 export { type PlanNode, todoItemsToPlanNodes } from "./todo-plan-nodes.js";
 export { createTodolistTool, type TodoItem, type TodolistTool } from "./todolist.js";
-export { type TruncationOptions, type TruncationResult, truncateOutput } from "./truncation.js";
+export {
+  type TruncationMode,
+  type TruncationOptions,
+  type TruncationResult,
+  truncateOutput,
+} from "./truncation.js";
 export { createUpdatePlanTool } from "./update-plan.js";
 export { type CreateWebFetchToolOptions, createWebFetchTool } from "./web-fetch.js";
 export {
